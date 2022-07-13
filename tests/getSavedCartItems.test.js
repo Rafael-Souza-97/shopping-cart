ee7bd625cb81fa3e3f843e5c75193a91d0fbed3a
Rafael Souza <souza_rafael.97@outlook.com>
2022-07-13 17:03:29 -0300

    Adiciona Requisitos 6 e 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -10,7 +10,7 @@ describe('4 - Teste a função getSavedCartItems', () => {
   });
 
   it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro', () => {
-    getSavedCartItems('cartItems');
+    getSavedCartItems();
     expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
   });
 });

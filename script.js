79b3518d584145b2db638822c85aa8096aa2df3a
Rafael Souza <souza_rafael.97@outlook.com>
2022-07-16 17:07:59 -0300

    Adiciona alterações no layout

--- script.js
+++ script.js
@@ -2,22 +2,51 @@ const items = document.querySelector('.items');
 const cartItems = document.querySelector('.cart__items');
 const emptyCart = document.querySelector('.empty-cart');
 const totalCart = document.querySelector('.total-price');
+const inputSearchDiv = document.querySelector('.inputSearch-div');
+const cartSession = document.querySelector('.cart');
+const loader = document.querySelector('.loader');
+const searchButton = document.querySelector('.buttonPesquisar');
+const inputSearch = document.querySelector('.input-product');
 
-const totalCartPrice = () => {
-  let value = 0;
-  cartItems.childNodes.forEach((element) => {
-    const strLi = element.innerText;
-    const strNum = Number(strLi.split('$')[1]);
-    value += strNum;
-  });
-  const total = Math.round((value + Number.EPSILON) * 100) / 100;
-  totalCart.innerText = total;
+const createCartItemElement = ({ name, salePrice, image }) => {
+  let button;
+  const div = document.createElement('div');
+  const li = document.createElement('li');
+  div.className = 'divItem-cart';
+  li.className = 'cart__item';
+  li.appendChild(div);
+  div.appendChild(createProductImageElement(image));
+  div.appendChild(createSpan('span', 'cart-name', name))
+  button = (createSpan('span', 'remove-product', "X"));
+  div.appendChild(button);
+  li.appendChild(createSpan('span', 'cartProduct-price', `${salePrice.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`))
+  button.addEventListener('click', cartItemClickListener);
+  return li;
 };
 
+const totalCartPrice = () => {
+  const cartItems = document.querySelector('.cart__items');
+  let value = 0;
+  cartItems.childNodes.forEach((element) => {
+    const str = element.lastElementChild.innerText;
+    const clean = str.replace(/[^0-9,]*/g, '').replace(',', '.');
+    console.log(clean);
+    value += Number(clean);
+  });
+  totalCart.innerHTML = `Subtotal (${cartLength()} item(s)): <strong> ${value.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</strong>`;
+}
+
+const cartLength = () => {
+  const cartLen = document.querySelector('.cart-length');
+  cartLen.innerText = cartItems.children.length;
+  return cartLen.innerText;
+}
+
 const createProductImageElement = (imageSource) => {
+  const imageHD = imageSource.replace(/I.jpg/g, 'W.jpg');
   const img = document.createElement('img');
   img.className = 'item__image';
-  img.src = imageSource;
+  img.src = imageHD;
   return img;
 };
 
@@ -28,49 +57,52 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
+const createProductItemElement = ({ sku, name, price, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createSpan('span', 'cart-price', `${price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
 };
 
-// const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+const recoveryButton = () => {
+  const removeButton = document.querySelectorAll('.remove-product');
+  removeButton.forEach((element) => element.addEventListener('click', cartItemClickListener))
+}
 
 const cartItemClickListener = (event) => {
-  cartItems.removeChild(event.target);
+  event.target.parentElement.parentElement.remove();
   saveCartItems(cartItems.innerHTML);
   totalCartPrice();
+  cartLength();
 };
 
-const emptyCartClickListener = () => {
-  emptyCart.addEventListener('click', () => {
-    cartItems.innerHTML = '';
-    saveCartItems(cartItems.innerHTML);
-    totalCartPrice();
-  });
-};
+emptyCart.addEventListener('click', () => {
+  cartItems.innerHTML = '';
+  saveCartItems(cartItems.innerHTML);
+  totalCartPrice();
+  cartLength();
+});
 
-const createCartItemElement = ({ sku, name, salePrice }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-};
+const createSpan = (element, className, text) => {
+  const e = document.createElement(element);
+  e.innerText = text;
+  e.className = className;
+  return e;
+}
 
 const skuToCart = async (button) => {
   const sku = button.parentElement.querySelector('.item__sku').innerText;
   const product = await fetchItem(sku);
-  const data = { sku: product.id, name: product.title, salePrice: product.price };
+  const data = { sku: product.id, name: product.title, salePrice: product.price, image: product.thumbnail };
   cartItems.appendChild(createCartItemElement(data));
   saveCartItems(cartItems.innerHTML);
   totalCartPrice();
+  cartLength();
 };
 
 const addCartBtn = () => {
@@ -80,19 +112,20 @@ const addCartBtn = () => {
   }));
 };
 
-const reqApi = async () => {
-  const products = await fetchProducts('computador');
+const reqApi = async (param) => {
+  const products = await fetchProducts(param);
   products.results.forEach((item) => {
-  const { id, title, thumbnail } = item;
+  const { id, title, thumbnail, price } = item;
   const createItem = createProductItemElement({
     sku: id,
     name: title,
+    price: price,
     image: thumbnail,
   });
   items.appendChild(createItem);
   });
   addCartBtn();
-  emptyCartClickListener();
+  recoveryButton();
 };
 
 const getItemsLocalStorage = () => {
@@ -101,30 +134,53 @@ const getItemsLocalStorage = () => {
 };
 
 const getAllEvents = () => {
-  const liCartItem = document.querySelectorAll('.cart__item');
-  liCartItem.forEach((element) => {
+  const removeButton = document.querySelectorAll('.remove-cart');
+  removeButton.forEach((element) => {
     element.addEventListener('click', cartItemClickListener);
   });
 };
 
+const cartDisplayNone = () => {
+  const cart = document.querySelector('.cart');
+  const cartIcon = document.querySelector('.cart-icon');
+  cartIcon.addEventListener('click',() => {
+    if (cart.style.display !== 'none') {
+      return cart.style.display = 'none';
+    }
+    if(cart.style.display !== 'flex') {
+      return cart.style.display = 'flex';
+    }
+  })
+}
+
+const searchBar = () => {
+  reqApi('iphone');
+  searchButton.addEventListener('click', async () => {
+    if(inputSearch.value === '') reqApi('computador');
+    if(inputSearch.value.toLowerCase() === 'taça do mundial do palmeiras') window.alert('ERRO 404 - O Palmeiras não tem Mundial');
+    items.innerHTML = '';
+    reqApi(inputSearch.value);
+    inputSearch.value = '';
+  })
+}
+
 const loading = () => {
-  const loadingText = document.createElement('h3');
-  loadingText.innerText = 'carregando...';
-  loadingText.classList = 'loading';
-  const body = document.querySelector('body');
-  body.appendChild(loadingText);
   items.style.display = 'none';
-  setTimeout(async () => {
+  inputSearchDiv.style.display = 'none';
+  cartSession.style.display = 'none'
+  setTimeout(() => {
     items.style.display = 'flex';
-    loadingText.style.display = 'flex';
-    await reqApi();
-    body.removeChild(loadingText);
+    inputSearchDiv.style.display = 'flex';
+    loader.style.display = 'none'
     getItemsLocalStorage();
     getAllEvents();
     totalCartPrice();
-  });
+    cartDisplayNone();
+    cartLength();
+    searchBar();
+  }, 1500);
 };
 
 window.onload = () => {
-loading();
-};
+  loading();
+}
